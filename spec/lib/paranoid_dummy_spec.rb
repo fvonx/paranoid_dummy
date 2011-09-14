27bd49c73a04144a536303124d95b279ab20340b
Matthias Stiller <matthias.stiller@metascape.de>
2011-09-14 12:02:34 +0200

    Added specs.

--- spec/lib/paranoid_dummy_spec.rb
+++ spec/lib/paranoid_dummy_spec.rb
@@ -1,8 +1,47 @@
 require 'spec_helper'
 
 describe ParanoidDummy do
-  context '' do
-    it 'does smthg' do
+
+  context '.define_paranoid_dummy' do
+    it 'extends class with class method dummy' do
+      User.should respond_to(:dummy)
+      User.dummy.should be_a_kind_of(ParanoidDummy::Dummy)
     end
   end
+
+  context 'associated object is nil' do
+    before(:each) do
+      @post_without_user = Post.create(subject: 'subject', body: 'body')
+      @dummy             = @post_without_user.user
+    end
+
+    it 'returns object of class ParanoidDummy::Dummy' do
+      @dummy.should be_a_kind_of(ParanoidDummy::Dummy)
+    end
+
+    it 'returns dummy object with predefined methods' do
+      @dummy.should respond_to(:name)
+      @dummy.name.should eql('deleted user')
+    end
+
+    it 'returns dummy object with predefined default return values' do
+      @dummy.foobar.should eql('foo')
+    end
+
+    it 'returns dummy object that allows arbitrary method calls' do
+      @dummy.amazing_method('foo', :bar, [], {}).should eql('foo')
+    end
+  end
+
+  context 'associated object is not nil' do
+    before(:each) do
+      @user           = User.create(name: 'foobar')
+      @post_with_user = Post.create(subject: 'subject', body: 'body', user: @user)
+    end
+
+    it 'returns associated object' do
+      @post_with_user.user.should be(@user)
+    end
+  end
+
 end
--- spec/support/models.rb
+++ spec/support/models.rb
@@ -1,11 +1,12 @@
+class Post < ActiveRecord::Base
+  belongs_to :user
+end
+
 class User < ActiveRecord::Base
   has_many :posts
 
   define_paranoid_dummy do
     responds_to :name, with: 'deleted user'
+    defaults_to 'foo'
   end
-end
-
-class Post < ActiveRecord::Base
-  belongs_to :user
 end
